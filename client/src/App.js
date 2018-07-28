5f4195bfe818456d787e14cec2511fe9c0fdd754
nikpap123 <32428793+nikpap123@users.noreply.github.com>
2018-07-28 14:44:24 -0700

    primary login ui

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,62 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+import "./App.css";
 
-class App extends Component {
-  state = { users: [] }
+export default class Login extends Component {
+  constructor(props) {
+    super(props);
 
-  componentDidMount() {
-    fetch('/users')
-      .then(res => res.json())
-      .then(users => this.setState({ users }));
+    this.state = {
+      email: "",
+      password: ""
+    };
   }
+
+  validateForm() {
+    return this.state.email.length > 0 && this.state.password.length > 0;
+  }
+
+  handleChange = event => {
+    this.setState({
+      [event.target.id]: event.target.value
+    });
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+  }
+
   render() {
     return (
-      <div className="App">
-        <h1>Users</h1>
-        <ul>
-          {this.state.users.map(user =>
-            <li key={user.id}>{user.username}</li>
-          )}
-        </ul>
+      <div className="Login">
+        <form onSubmit={this.handleSubmit}>
+          <FormGroup controlId="email" bsSize="large">
+            <ControlLabel>Email</ControlLabel>
+            <FormControl
+              autoFocus
+              type="email"
+              value={this.state.email}
+              onChange={this.handleChange}
+            />
+          </FormGroup>
+          <FormGroup controlId="password" bsSize="large">
+            <ControlLabel>Password</ControlLabel>
+            <FormControl
+              value={this.state.password}
+              onChange={this.handleChange}
+              type="password"
+            />
+          </FormGroup>
+          <Button
+            block
+            bsSize="large"
+            disabled={!this.validateForm()}
+            type="submit"
+          >
+            Login
+          </Button>
+        </form>
       </div>
     );
   }
 }
-
-export default App;
